0f8af57cb7dcfc0b177eeeb5e69a26ec77431d14
“chrisshank” <chris.shank.23@gmail.com>
2024-09-13 09:48:28 -0700

    combine tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,16 +4,16 @@ import { RouteData, RouteParser, boolean, date, datetime, num, route, string } f
 
 interface DecodeFixture {
   route: () => RouteParser;
-  input: string;
-  expectedMatch: ReturnType<RouteParser['decode']>;
+  url: string;
+  data: RouteData<Record<string, any>> | null;
 }
 
 // Adapted from the URLPattern test suite: https://github.com/kenchris/urlpattern-polyfill/blob/main/test/urlpatterntestdata.json
-const URLPatternDecoderFixtures: DecodeFixture[] = [
+const URLPatternFixtures: DecodeFixture[] = [
   {
     route: () => route`/foo/bar`,
-    input: '/foo/bar',
-    expectedMatch: {
+    url: '/foo/bar',
+    data: {
       params: {},
       search: {},
       hash: '',
@@ -21,23 +21,23 @@ const URLPatternDecoderFixtures: DecodeFixture[] = [
   },
   {
     route: () => route`/foo/bar`,
-    input: '/foo/ba',
-    expectedMatch: null,
+    url: '/foo/ba',
+    data: null,
   },
   {
     route: () => route`/foo/bar`,
-    input: '/foo/bar/',
-    expectedMatch: null,
+    url: '/foo/bar/',
+    data: null,
   },
   {
     route: () => route`/foo/bar`,
-    input: '/foo/bar/baz',
-    expectedMatch: null,
+    url: '/foo/bar/baz',
+    data: null,
   },
   {
     route: () => route`/foo/${string('bar')}`,
-    input: '/foo/bar',
-    expectedMatch: {
+    url: '/foo/bar',
+    data: {
       params: { bar: 'bar' },
       search: {},
       hash: '',
@@ -45,8 +45,8 @@ const URLPatternDecoderFixtures: DecodeFixture[] = [
   },
   {
     route: () => route`/foo/${string('bar')}`,
-    input: '/foo/index.html',
-    expectedMatch: {
+    url: '/foo/index.html',
+    data: {
       params: { bar: 'index.html' },
       search: {},
       hash: '',
@@ -54,18 +54,18 @@ const URLPatternDecoderFixtures: DecodeFixture[] = [
   },
   {
     route: () => route`/foo/${string('bar')}`,
-    input: '/foo/',
-    expectedMatch: null,
+    url: '/foo/',
+    data: null,
   },
   {
     route: () => route`/foo/${string('bar')}`,
-    input: '/foo/bar/',
-    expectedMatch: null,
+    url: '/foo/bar/',
+    data: null,
   },
   {
     route: () => route`/${string('café')}`,
-    input: '/foo',
-    expectedMatch: {
+    url: '/foo',
+    data: {
       params: { café: 'foo' },
       search: {},
       hash: '',
@@ -73,8 +73,8 @@ const URLPatternDecoderFixtures: DecodeFixture[] = [
   },
   {
     route: () => route`/${string('\u2118')}`,
-    input: '/foo',
-    expectedMatch: {
+    url: '/foo',
+    data: {
       params: { '\u2118': 'foo' },
       search: {},
       hash: '',
@@ -82,8 +82,8 @@ const URLPatternDecoderFixtures: DecodeFixture[] = [
   },
   {
     route: () => route`/${string('\u3400')}`,
-    input: '/foo',
-    expectedMatch: {
+    url: '/foo',
+    data: {
       params: { '\u3400': 'foo' },
       search: {},
       hash: '',
@@ -91,8 +91,8 @@ const URLPatternDecoderFixtures: DecodeFixture[] = [
   },
   {
     route: () => route`/foo/bar`,
-    input: '/foo/./bar',
-    expectedMatch: {
+    url: '/foo/./bar',
+    data: {
       params: {},
       search: {},
       hash: '',
@@ -100,8 +100,8 @@ const URLPatternDecoderFixtures: DecodeFixture[] = [
   },
   {
     route: () => route`/foo/baz`,
-    input: '/foo/bar/../baz',
-    expectedMatch: {
+    url: '/foo/bar/../baz',
+    data: {
       params: {},
       search: {},
       hash: '',
@@ -109,8 +109,8 @@ const URLPatternDecoderFixtures: DecodeFixture[] = [
   },
   {
     route: () => route`/caf%C3%A9`,
-    input: '/café',
-    expectedMatch: {
+    url: '/café',
+    data: {
       params: {},
       search: {},
       hash: '',
@@ -128,14 +128,14 @@ const URLPatternDecoderFixtures: DecodeFixture[] = [
   }, */
   {
     route: () => route`/caf%c3%a9`,
-    input: '/café',
-    expectedMatch: null,
+    url: '/café',
+    data: null,
   },
   // Note URLPattern doesn't match here
   {
     route: () => route`/foo/bar`,
-    input: 'foo/bar',
-    expectedMatch: {
+    url: 'foo/bar',
+    data: {
       params: {},
       search: {},
       hash: '',
@@ -164,8 +164,8 @@ const URLPatternDecoderFixtures: DecodeFixture[] = [
   **/
   {
     route: () => route``,
-    input: '/',
-    expectedMatch: {
+    url: '/',
+    data: {
       params: {},
       search: {},
       hash: '',
@@ -173,8 +173,8 @@ const URLPatternDecoderFixtures: DecodeFixture[] = [
   },
   {
     route: () => route`${string('name')}.html`,
-    input: 'foo.html',
-    expectedMatch: {
+    url: 'foo.html',
+    data: {
       params: { name: 'foo' },
       search: {},
       hash: '',
@@ -182,8 +182,8 @@ const URLPatternDecoderFixtures: DecodeFixture[] = [
   },
   {
     route: () => route`/foo!`,
-    input: '/foo!',
-    expectedMatch: {
+    url: '/foo!',
+    data: {
       params: {},
       search: {},
       hash: '',
@@ -191,11 +191,12 @@ const URLPatternDecoderFixtures: DecodeFixture[] = [
   },
 ];
 
-const decoderFixtures: DecodeFixture[] = [
+const fixtures: DecodeFixture[] = [
+  ...URLPatternFixtures,
   {
     route: () => route`/${num('num')}`,
-    input: '/1',
-    expectedMatch: {
+    url: '/1',
+    data: {
       params: { num: 1 },
       search: {},
       hash: '',
@@ -203,8 +204,8 @@ const decoderFixtures: DecodeFixture[] = [
   },
   {
     route: () => route`/${num('float')}`,
-    input: '/1.01',
-    expectedMatch: {
+    url: '/1.01',
+    data: {
       params: { float: 1.01 },
       search: {},
       hash: '',
@@ -212,8 +213,8 @@ const decoderFixtures: DecodeFixture[] = [
   },
   {
     route: () => route`/${boolean('boolean')}`,
-    input: '/true',
-    expectedMatch: {
+    url: '/true',
+    data: {
       params: { boolean: true },
       search: {},
       hash: '',
@@ -221,8 +222,8 @@ const decoderFixtures: DecodeFixture[] = [
   },
   {
     route: () => route`/${boolean('boolean')}`,
-    input: '/false',
-    expectedMatch: {
+    url: '/false',
+    data: {
       params: { boolean: false },
       search: {},
       hash: '',
@@ -230,17 +231,17 @@ const decoderFixtures: DecodeFixture[] = [
   },
   {
     route: () => route`/${date('date')}`,
-    input: '/01-01-2000',
-    expectedMatch: {
-      params: { date: new Date('01-01-2000') },
+    url: '/2000-01-01',
+    data: {
+      params: { date: new Date('2000-01-01') },
       search: {},
       hash: '',
     },
   },
   {
     route: () => route`/${datetime('datetime')}`,
-    input: '/2000-01-01T07%3A07%3A06.664Z',
-    expectedMatch: {
+    url: '/2000-01-01T07%3A07%3A06.664Z',
+    data: {
       params: { datetime: new Date('2000-01-01T07:07:06.664Z') },
       search: {},
       hash: '',
@@ -248,161 +249,16 @@ const decoderFixtures: DecodeFixture[] = [
   },
 ];
 
-const decodeFixtures = [...URLPatternDecoderFixtures, ...decoderFixtures];
+for (const { route, url, data } of fixtures) {
+  test(`'${url}' for ${route}`, () => {
+    assert.deepStrictEqual(route().decode(url), data);
 
-for (const { route, input, expectedMatch } of decodeFixtures) {
-  test(`Decode: '${input}' for ${route}`, () => {
-    assert.deepStrictEqual(route().decode(input), expectedMatch);
-  });
-}
-
-interface EncodeFixture {
-  route: () => RouteParser;
-  input: RouteData<Record<string, any>>;
-  expectedMatch: string;
-}
-
-// Adapted from the URLPattern test suite: https://github.com/kenchris/urlpattern-polyfill/blob/main/test/urlpatterntestdata.json
-const URLPatternEncoderFixtures: EncodeFixture[] = [
-  {
-    route: () => route`/foo/bar`,
-    input: {
-      params: {},
-    },
-    expectedMatch: '/foo/bar',
-  },
-  {
-    route: () => route`/foo/${['bar', string]}`,
-    input: {
-      params: { bar: 'bar' },
-    },
-    expectedMatch: '/foo/bar',
-  },
-  {
-    route: () => route`/foo/${['bar', string]}`,
-    input: {
-      params: { bar: 'index.html' },
-    },
-    expectedMatch: '/foo/index.html',
-  },
-  {
-    route: () => route`/${['café', string]}`,
-    input: {
-      params: { café: 'foo' },
-    },
-    expectedMatch: '/foo',
-  },
-  {
-    route: () => route`/${['\u2118', string]}`,
-    input: {
-      params: { '\u2118': 'foo' },
-    },
-    expectedMatch: '/foo',
-  },
-  {
-    route: () => route`/${['\u3400', string]}`,
-    input: {
-      params: { '\u3400': 'foo' },
-    },
-    expectedMatch: '/foo',
-  },
-  {
-    route: () => route`/café`,
-    input: {
-      params: {},
-    },
-    expectedMatch: '/caf%C3%A9',
-  },
-  /* Support relative paths in definition?
-   * This would require parsing the URL before generating the regex.
-  {
-    route: () => route`/foo/../bar`,
-    input: '/bar',
-    expectedMatch: {
-      params: {},
-    },
-  },
-   {
-    route: () => route`./foo/bar`,
-    input: 'foo/bar',
-    expectedMatch: {
-      params: {},
-    },
-  },
-  **/
-  {
-    route: () => route``,
-    input: {
-      params: {},
-    },
-    expectedMatch: '/',
-  },
-  {
-    route: () => route`${['name', string]}.html`,
-    input: {
-      params: { name: 'foo' },
-    },
-    expectedMatch: '/foo.html',
-  },
-  {
-    route: () => route`/foo!`,
-    input: {
-      params: {},
-    },
-    expectedMatch: '/foo!',
-  },
-];
-
-const encoderFixtures: EncodeFixture[] = [
-  {
-    route: () => route`/${['num', num]}`,
-    input: {
-      params: { num: 1 },
-    },
-    expectedMatch: '/1',
-  },
-  {
-    route: () => route`/${['float', num]}`,
-    input: {
-      params: { float: 1.01 },
-    },
-    expectedMatch: '/1.01',
-  },
-  {
-    route: () => route`/${['boolean', boolean]}`,
-    input: {
-      params: { boolean: true },
-    },
-    expectedMatch: '/true',
-  },
-  {
-    route: () => route`/${['boolean', boolean]}`,
-    input: {
-      params: { boolean: false },
-    },
-    expectedMatch: '/false',
-  },
-  {
-    route: () => route`/${['date', date]}`,
-    input: {
-      params: { date: new Date('2000-01-01') },
-    },
-    expectedMatch: '/2000-01-01',
-  },
-  {
-    route: () => route`/${['datetime', datetime]}`,
-    input: {
-      params: { datetime: new Date('2000-01-01T07:07:06.664Z') },
-    },
-    expectedMatch: '/2000-01-01T07%3A07%3A06.664Z',
-  },
-];
-
-const encodeFixtures = [...URLPatternEncoderFixtures, ...encoderFixtures];
-
-for (const { route, input, expectedMatch } of encodeFixtures) {
-  test(`Encode: '${expectedMatch}' for ${route}`, () => {
-    assert.deepStrictEqual(route().encode(input), expectedMatch);
+    if (data !== null) {
+      // `encode` always outputs a normalized URL, so we want to assert a normalized url is outputted
+      // Does this make sense?
+      const parsedURL = new URL(url, 'https://a.com');
+      assert.deepStrictEqual(route().encode(data), parsedURL.pathname);
+    }
   });
 }
 
